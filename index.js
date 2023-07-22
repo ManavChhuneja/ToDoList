581b4f169428092fcf29c51ef363309827d88acd
Manav Chhuneja <manavchhuneja64@gmail.com>
2023-07-22 13:44:46 -0400

    adding remaining logic for the app

--- index.js
+++ index.js
@@ -2,12 +2,30 @@ import express from "express";
 
 const app = express();
 const port = 3000;
+const dateToday = new Date().toDateString();
+let toDoList = [];
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  toDoList = []
+  
+  res.render("index.ejs", {
+    date: dateToday, 
+    toDoList: toDoList
+  });
 });
+
+app.post("/", (req, res) => {
+  const todo = req.body.todo;
+  toDoList.push(todo);
+
+  res.render("index.ejs", {
+    toDoList: toDoList, 
+    date: dateToday
+  })
+})
 
 app.listen(port, () => {
   console.log(`Server live on port ${port}`);
